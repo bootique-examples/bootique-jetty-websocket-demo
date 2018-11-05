20747a0414b6b6b34779c66c8d1b4809894afd4c
kkomyak <const1993@gmail.com>
2018-11-05 10:32:39 +0300

    Added bootique-jetty-websocket-demo

---
+++ src/main/java/io/bootique/websoket/demo/MessageUtil.java
@@ -0,0 +1,40 @@
+package io.bootique.websoket.demo;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MessageUtil {
+
+    public static final String SYMBOL = "symbol";
+    public static final String SCHEMA = "schema";
+    public static final String MAX_FREQUENCY = "maxFrequency";
+    public static final String COMMAND = "command";
+
+    public static final String SUBSCRIBE = "subscribe";
+    public static final String UNSUBSCRIBE = "unsubscribe";
+
+    public static Map<String, Object> parseJson(String message) {
+       Map<String, Object> result = new Gson().fromJson(message, HashMap.class);
+       result.putIfAbsent(MAX_FREQUENCY, Double.MAX_VALUE);
+       result.putIfAbsent(SCHEMA, new ArrayList<>());
+       return result;
+    }
+
+    public static String toJson(Map<String, String> message) {
+        Map<String, String> toSend = new HashMap<>();
+        // Beautifying JSON
+        message.forEach((k, v) -> toSend.put(k.toLowerCase().replace("_", ""), v));
+
+        return new Gson().toJson(toSend);
+    }
+
+    public static void toLowerCase(Map<String, String> data) {
+        Map<String, String> result = new HashMap<>();
+        data.forEach((k, v) -> result.put(k.toLowerCase().replace("_", ""), v));
+        data.clear();
+        data.putAll(result);
+    }
+}
